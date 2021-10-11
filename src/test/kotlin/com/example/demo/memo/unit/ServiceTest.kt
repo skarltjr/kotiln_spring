08dcb6da62efa11310f9b035f69b2dfe9e57b5d5
skarltjr <kisa0828@naver.com>
2021-10-12 00:54:06 +0900

    fix(test) : edit memo unit test

--- src/test/kotlin/com/example/demo/memo/unit/ServiceTest.kt
+++ src/test/kotlin/com/example/demo/memo/unit/ServiceTest.kt
@@ -8,6 +8,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito
 import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
@@ -33,21 +34,30 @@ class ServiceTest {
             title = "test",
             text = "test"
         )
+        val memo = Memo(
+            id = 1L,
+            title = memoDto.title,
+            text = memoDto.text,
+            createdAt = LocalDateTime.now(),
+            updatedAt = null
+        )
 
-        // given - stubbing 앞으로 이런게 불리면 이 값을 줘
-        given(memoRepository.findById(1)).willReturn(returnOptionalMemo(1,memoDto))
+        // service => 사용하는 repository에 대해선 모킹을 해줘야한다
+        given(memoRepository.save(BDDMockito.any())).willReturn(memo) // save에 대해
+        given(memoRepository.findById(1)).willReturn(returnOptionalMemo(memo)) // find에 대해
 
         //when
-        memoService.createMemo(memoDto)
+        val createMemo = memoService.createMemo(memoDto)
         val findById = memoRepository.findById(1)
-        //then 예측과 비교
-        assertThat(findById).isEqualTo(returnOptionalMemo(1,memoDto))
+        //then
+        assertThat(createMemo.body?.content?.id).isEqualTo(findById.get().id)
+        assertThat(createMemo.body?.content?.title).isEqualTo(findById.get().title)
+        assertThat(createMemo.body?.content?.text).isEqualTo(findById.get().text)
+        assertThat(createMemo.body?.content?.createdAt).isEqualTo(findById.get().createdAt)
+        assertThat(createMemo.body?.content?.updatedAt).isEqualTo(findById.get().updatedAt)
     }
-    private fun returnOptionalMemo(id: Long,memoDto: MemoDto): Optional<Memo> {
-        return Optional.of(createMemo(id,memoDto))
+    private fun returnOptionalMemo(memo:Memo): Optional<Memo> {
+        return Optional.of(memo)
     }
 
-    private fun createMemo(id: Long, memoDto: MemoDto): Memo {
-        return Memo(id,memoDto.title,memoDto.text, LocalDateTime.now())
-    }
 }
